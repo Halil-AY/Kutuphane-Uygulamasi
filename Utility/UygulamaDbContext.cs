a8820be007d497ecce87c1f7683df74ac00beccc
Halil AY <halil3443@gmail.com>
2023-08-24 00:32:22 +0300

    ASP.Net 7 Core MVC Kütüphane Uygulaması
    ASP.Net 7 Core MVC ve Entity Framework ile oluşturduğum Online Kütüphane Uygulaması

---
+++ Utility/UygulamaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebUygulamaProje1.Models;
+
+// Veri Tabanında EF Tablo Oluşturması için ilgili model sınıflaırnızı buraya eklemelisiniz..
+namespace WebUygulamaProje1.Utility
+{
+    public class UygulamaDbContext : IdentityDbContext
+    {
+
+        public UygulamaDbContext(DbContextOptions<UygulamaDbContext> options) : base(options) { }
+
+		public DbSet<KitapTuru> KitapTurleri { get; set; }
+        public DbSet<Kitap> Kitaplar { get; set; }
+		public DbSet<Kiralama> Kiralamalar { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }    
+
+	}
+}
